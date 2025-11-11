6b940a60bf6b0e757799a8e1ab3ebd9810128ec9
SowmyaPotnuru <sowmyapotnuru781@gmail.com>
2025-11-11 17:29:43 +0530

    Basic Programming is created

---
+++ index.js
@@ -0,0 +1,18 @@
+function checkIsPrime(num){
+  let isPrime = true 
+  for(let i=2; i<num; i++){
+    if(num%i == 0){
+      isPrime = false
+      break
+    }
+  }
+  if(isPrime){
+    console.log(true)
+  }
+  else{
+    console.log(false)
+  }
+}
+
+checkIsPrime(4)
+checkIsPrime(31)
